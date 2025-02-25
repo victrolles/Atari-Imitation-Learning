3cd58faf6d1af19777befa3c0f67d4143f60976b
Victor Goudal <95492416+victrolles@users.noreply.github.com>
2025-02-25 09:32:47 -0500

    rework to mp

--- test_dqn_mp.py
+++ test_dqn_mp.py
@@ -1,23 +1,13 @@
-import multiprocessing
-from _dqn_on_gym import DQNOnGym
+import multiprocessing as mp
 
-def train_agent(process_id):
-    """Trains a DQN agent on the MsPacman-v4 environment."""
-    dqn_on_gym = DQNOnGym()
-    dqn_on_gym.train_loop()
-    print(f"Process {process_id} finished training.")
+NUM_WORKER_ENV = 3
+NUM_WORKER_TRAINER = 2
 
-def main(num_processes=4):
-    """Runs multiple processes to train DQN agents in parallel."""
-    processes = []
-    for i in range(num_processes):
-        p = multiprocessing.Process(target=train_agent, args=(i,))
-        p.start()
-        processes.append(p)
+def main():
+    queue = mp.Queue()
+
+    for idx in range()
     
-    for p in processes:
-        p.join()
 
 if __name__ == "__main__":
-    num_processes =11
-    main(num_processes)
+    main()
